67c350c3892f18f6a26c40f3c8f0292511159ef7
Leonardo Gurgel <lgurgel@id.uff.br>
2019-10-03 08:51:32 -0300

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: [
-    'standard'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: ['standard'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'space-before-function-paren': ['error', 'never'],
+		indent: ['error', 'tab'],
+		'no-tabs': 'off',
+		'react/jsx-indent': ['error', 'tab'],
+    'react/jsx-indent-props': ['error', 'tab'],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+		quotes: ['error', 'single']
+	}
 }
